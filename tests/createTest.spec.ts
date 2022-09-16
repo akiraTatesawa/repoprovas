56fa6aaea12d15723db709167adfaab7fc7ad2e3
Arthur Akira <arthurak10.at@gmail.com>
2022-09-15 21:54:25 -0300

    chore: create seed

--- tests/createTest.spec.ts
+++ tests/createTest.spec.ts
@@ -6,18 +6,16 @@ import { UserFactory } from "./factories/userFactory";
 
 beforeEach(async () => {
   await prisma.$executeRaw`TRUNCATE TABLE tests;`;
+  await prisma.$executeRaw`TRUNCATE TABLE users;`;
 });
 
 afterAll(async () => {
+  await prisma.$executeRaw`TRUNCATE TABLE tests;`;
   await prisma.$executeRaw`TRUNCATE TABLE users;`;
   await prisma.$disconnect();
 });
 
 describe("POST /tests", () => {
-  let token = {};
-  const invalidTokenFormat = {
-    Authorization: "invalid",
-  };
   it("Should be able to create a test", async () => {
     const { email, password, hashedPassword } = new UserFactory().createUser();
     await prisma.user.create({
@@ -30,7 +28,7 @@ describe("POST /tests", () => {
       .post("/sign-in")
       .send({ email, password });
 
-    token = {
+    const token = {
       Authorization: `Bearer ${resultSignIn.body.token}`,
     };
 
@@ -46,6 +44,21 @@ describe("POST /tests", () => {
   });
 
   it("Should not be able to create a test with an invalid test format", async () => {
+    const { email, password, hashedPassword } = new UserFactory().createUser();
+    await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+    const resultSignIn = await supertest(server)
+      .post("/sign-in")
+      .send({ email, password });
+
+    const token = {
+      Authorization: `Bearer ${resultSignIn.body.token}`,
+    };
+
     const resultTest = await supertest(server).post("/tests").set(token).send({
       name: "test name",
       categoryId: 1,
@@ -55,6 +68,7 @@ describe("POST /tests", () => {
     });
 
     expect(resultTest.status).toEqual(422);
+    expect(resultTest.body).toBeInstanceOf(Object);
   });
 
   it("Should not be able to create a test without a token", async () => {
@@ -66,9 +80,14 @@ describe("POST /tests", () => {
     });
 
     expect(resultTest.status).toEqual(400);
+    expect(resultTest.body).toBeInstanceOf(Object);
   });
 
   it("Should not be able to create a test with an invalid token format", async () => {
+    const invalidTokenFormat = {
+      Authorization: "invalid",
+    };
+
     const resultTest = await supertest(server)
       .post("/tests")
       .set(invalidTokenFormat)
@@ -80,6 +99,7 @@ describe("POST /tests", () => {
       });
 
     expect(resultTest.status).toEqual(422);
+    expect(resultTest.body).toBeInstanceOf(Object);
   });
 
   it("Should not be able to create a test with an invalid token", async () => {
@@ -96,5 +116,6 @@ describe("POST /tests", () => {
       });
 
     expect(resultTest.status).toEqual(401);
+    expect(resultTest.body).toBeInstanceOf(Object);
   });
 });
